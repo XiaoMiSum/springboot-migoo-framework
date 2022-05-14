29f42b2949fbf7ebe1cdf26ee8b284a477ebc137
xiaomi <mi_xiao@qq.com>
2022-05-14 13:59:37 +0800

    fix: springboot scheduler 任务并行

--- migoo-framework-parent/migoo-spring-boot-starter-job/src/main/java/xyz/migoo/framework/quartz/config/MiGooQuartzAutoConfiguration.java
+++ migoo-framework-parent/migoo-spring-boot-starter-job/src/main/java/xyz/migoo/framework/quartz/config/MiGooQuartzAutoConfiguration.java
@@ -4,15 +4,29 @@ import org.quartz.Scheduler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 import xyz.migoo.framework.quartz.core.scheduler.SchedulerManager;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 @Configuration
 @EnableScheduling // 开启 Spring 自带的定时任务
-public class MiGooQuartzAutoConfiguration {
+public class MiGooQuartzAutoConfiguration implements SchedulingConfigurer {
 
     @Bean
     public SchedulerManager schedulerManager(Scheduler scheduler) {
         return new SchedulerManager(scheduler);
     }
 
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        taskRegistrar.setScheduler(taskExecutor());
+    }
+
+    @Bean
+    public Executor taskExecutor() {
+        return Executors.newScheduledThreadPool(5);
+    }
 }
