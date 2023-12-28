dea0ac9916862a964a9ab69f77315e3dd6d014de
xiaomi <mi_xiao@qq.com>
2023-12-28 19:30:54 +0800

    🌱: 云服务商模块

--- migoo-framework-parent/migoo-spring-boot-starter-cvs/src/main/java/xyz/migoo/framework/cvs/config/MiGooCloudServiceAutoConfiguration.java
+++ migoo-framework-parent/migoo-spring-boot-starter-cvs/src/main/java/xyz/migoo/framework/cvs/config/MiGooCloudServiceAutoConfiguration.java
@@ -1,7 +1,6 @@
 package xyz.migoo.framework.cvs.config;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.context.annotation.Bean;
 
 /**
  * 短信配置类
@@ -10,10 +9,5 @@ import org.springframework.context.annotation.Bean;
  */
 @AutoConfiguration
 public class MiGooCloudServiceAutoConfiguration {
-
-    @Bean
-    public CloudServiceClientFactory cloudServiceClientFactory() {
-        return new CloudServiceClientFactoryImpl();
-    }
-
+    
 }
