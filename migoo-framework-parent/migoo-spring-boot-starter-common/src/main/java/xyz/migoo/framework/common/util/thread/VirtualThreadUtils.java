084c0e368cec97dfcbbfc88803df8814e526c203
xiaomi <mi_xiao@qq.com>
2024-05-09 19:44:25 +0800

    ✨框架更新

---
+++ migoo-framework-parent/migoo-spring-boot-starter-common/src/main/java/xyz/migoo/framework/common/util/thread/VirtualThreadUtils.java
@@ -0,0 +1,27 @@
+package xyz.migoo.framework.common.util.thread;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class VirtualThreadUtils {
+    
+    private static final ExecutorService executor;
+
+    static {
+        var factory = Thread.ofVirtual().name("BizVirtualThread", 1).factory();
+        executor = Executors.newThreadPerTaskExecutor(factory);
+    }
+
+    private VirtualThreadUtils() {
+    }
+
+    public static Future<?> submit(Runnable task) {
+        return executor.submit(task);
+    }
+
+    public static <T> Future<T> submit(Callable<T> task) {
+        return executor.submit(task);
+    }
+}
