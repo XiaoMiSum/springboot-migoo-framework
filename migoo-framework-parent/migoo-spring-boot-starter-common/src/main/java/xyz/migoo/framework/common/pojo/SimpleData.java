86323e2c532f003836352a43476dd63860cbed0e
xiaomi <mi_xiao@qq.com>
2024-05-24 18:06:28 +0800

    ✨优化字典功能

--- migoo-framework-parent/migoo-spring-boot-starter-common/src/main/java/xyz/migoo/framework/common/pojo/SimpleData.java
+++ migoo-framework-parent/migoo-spring-boot-starter-common/src/main/java/xyz/migoo/framework/common/pojo/SimpleData.java
@@ -4,14 +4,29 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class SimpleData<T> {
 
-    private T code;
+    private T value;
 
-    private String name;
+    private String label;
 
     private boolean disable;
+
+    private List<SimpleData<T>> children;
+
+    public SimpleData(T value, String label) {
+        this.value = value;
+        this.label = label;
+    }
+
+    public SimpleData(T value, String label, boolean disable) {
+        this.value = value;
+        this.label = label;
+        this.disable = disable;
+    }
 }
