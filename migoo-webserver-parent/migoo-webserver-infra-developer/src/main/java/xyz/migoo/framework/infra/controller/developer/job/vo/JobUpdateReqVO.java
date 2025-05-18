be054feb8a48623d86ceb386b7bcbc180a47b590
xiaomi <mi_xiao@qq.com>
2025-05-18 22:07:30 +0800

    :bug: 修复bug

--- migoo-webserver-parent/migoo-webserver-infra-developer/src/main/java/xyz/migoo/framework/infra/controller/developer/job/vo/JobUpdateReqVO.java
+++ migoo-webserver-parent/migoo-webserver-infra-developer/src/main/java/xyz/migoo/framework/infra/controller/developer/job/vo/JobUpdateReqVO.java
@@ -1,5 +1,6 @@
 package xyz.migoo.framework.infra.controller.developer.job.vo;
 
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -12,5 +13,8 @@ public class JobUpdateReqVO extends JobBaseVO {
 
     @NotNull(message = "任务编号不能为空")
     private Long id;
+
+    @NotEmpty(message = "处理器的名字不能为空")
+    private String handlerName;
 
 }
