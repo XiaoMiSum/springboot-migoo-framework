ce52fd04eeda41fd55065fc11550f7e51c4631db
xiaomi <mi_xiao@qq.com>
2026-07-14 17:16:18 +0800

    :bug: fix CacheUtils thread pool leak - use shared executor instead of creating new pool per call

--- migoo-framework-parent/migoo-spring-boot-starter-common/src/main/java/xyz/migoo/framework/common/util/cache/CacheUtils.java
+++ migoo-framework-parent/migoo-spring-boot-starter-common/src/main/java/xyz/migoo/framework/common/util/cache/CacheUtils.java
@@ -7,6 +7,7 @@ import com.google.common.cache.LoadingCache;
 
 import java.time.Duration;
 import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 /**
@@ -16,14 +17,15 @@ import java.util.concurrent.Executors;
  */
 public class CacheUtils {
 
+    private static final ExecutorService SHARED_EXECUTOR = Executors.newCachedThreadPool(
+            TtlExecutors.getDefaultDisableInheritableThreadFactory());
+
     public static <K, V> LoadingCache<K, V> buildAsyncReloadingCache(Duration duration, CacheLoader<K, V> loader) {
-        Executor executor = Executors.newCachedThreadPool(
-                TtlExecutors.getDefaultDisableInheritableThreadFactory()); // TTL 保证 ThreadLocal 可以透传
         return CacheBuilder.newBuilder()
                 // 只阻塞当前数据加载线程，其他线程返回旧值
                 .refreshAfterWrite(duration)
                 // 通过 asyncReloading 实现全异步加载，包括 refreshAfterWrite 被阻塞的加载线程
-                .build(CacheLoader.asyncReloading(loader, executor));
+                .build(CacheLoader.asyncReloading(loader, SHARED_EXECUTOR));
     }
 
 }
