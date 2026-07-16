4eaf98884522312106bfb1f9a80d00b696ca7876
xiaomi <mi_xiao@qq.com>
2026-07-16 17:36:15 +0800

    🗑️ 删除无用文件和过时接口
    - 删除 BaseUser, IBaseUserService (未使用的抽象类)
    - 删除 Base32.java (改用 commons-codec)
    - 删除 SecurityAuthFrameworkService (重命名为 AuthUserDetailsService)
    - 删除 SecurityAuthenticatorService (功能移入 TotpService)
    - 删除 SecurityPermissionFrameworkService (未使用)
    - 删除 JWTAuthenticationTokenFilter (重命名为 JwtAuthenticationFilter)
    - 删除 AuthenticatorInterceptor (重命名为 TotpInterceptor)
    - 删除 TokenMethodArgumentResolver (未使用)
    - 删除 Authenticated, AuthenticatorDTO (重命名为 LoginResult, TotpBinding)
    - 删除 GoogleAuthenticator (改用 java-otp)
    - 删除 @Authenticator, @Token 注解 (重命名为 @RequiresTotp)

--- migoo-framework-parent/migoo-spring-boot-starter-security/src/main/java/xyz/migoo/framework/security/core/service/IBaseUserService.java
+++
@@ -1,8 +0,0 @@
-package xyz.migoo.framework.security.core.service;
-
-import xyz.migoo.framework.security.core.BaseUser;
-
-public interface IBaseUserService<T, U> {
-
-    T toLoginUser(BaseUser<U> baseUser);
-}
