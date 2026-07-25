dc93c840819a956826ebb19797cf71568370ed3d
xiaomi <mi_xiao@qq.com>
2026-07-25 16:58:23 +0800

    :bug: fix(mybatis): uuid handler

--- migoo-framework-parent/migoo-spring-boot-starter-mybatis/src/main/java/xyz/migoo/framework/mybatis/core/handler/UUIDTypeHandler.java
+++ migoo-framework-parent/migoo-spring-boot-starter-mybatis/src/main/java/xyz/migoo/framework/mybatis/core/handler/UUIDTypeHandler.java
@@ -15,35 +15,60 @@ import java.util.UUID;
  * UUID 类型处理器
  * <p>
  * 支持 UUID 与数据库之间的转换
- * - 数据库存储为 CHAR(36) 或 VARCHAR(36) 格式
+ * - MySQL 等: 存储为 CHAR(36) 或 VARCHAR(36) 格式
+ * - PostgreSQL: 原生 uuid 类型
  * - 读取时自动转换为 UUID 对象
  *
  * @author xiaomi
  */
 @MappedTypes(UUID.class)
-@MappedJdbcTypes({JdbcType.CHAR, JdbcType.VARCHAR})
+@MappedJdbcTypes({JdbcType.CHAR, JdbcType.VARCHAR, JdbcType.OTHER, JdbcType.BINARY})
 public class UUIDTypeHandler extends BaseTypeHandler<UUID> {
 
     @Override
     public void setNonNullParameter(PreparedStatement ps, int i, UUID parameter, JdbcType jdbcType) throws SQLException {
-        ps.setString(i, parameter.toString());
+        if (jdbcType == JdbcType.OTHER) {
+            // PostgreSQL 原生 uuid 类型，使用 setObject 让 JDBC 驱动处理
+            ps.setObject(i, parameter);
+        } else {
+            // MySQL 等字符串存储
+            ps.setString(i, parameter.toString());
+        }
     }
 
     @Override
     public UUID getNullableResult(ResultSet rs, String columnName) throws SQLException {
-        String value = rs.getString(columnName);
-        return value != null ? UUID.fromString(value) : null;
+        Object value = rs.getObject(columnName);
+        if (value == null) {
+            return null;
+        }
+        if (value instanceof UUID) {
+            return (UUID) value;
+        }
+        return UUID.fromString(value.toString());
     }
 
     @Override
     public UUID getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
-        String value = rs.getString(columnIndex);
-        return value != null ? UUID.fromString(value) : null;
+        Object value = rs.getObject(columnIndex);
+        if (value == null) {
+            return null;
+        }
+        if (value instanceof UUID) {
+            return (UUID) value;
+        }
+        return UUID.fromString(value.toString());
     }
 
     @Override
     public UUID getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
-        String value = cs.getString(columnIndex);
-        return value != null ? UUID.fromString(value) : null;
+        Object value = cs.getObject(columnIndex);
+        if (value == null) {
+            return null;
+        }
+        if (value instanceof UUID) {
+            return (UUID) value;
+        }
+        return UUID.fromString(value.toString());
     }
 }
